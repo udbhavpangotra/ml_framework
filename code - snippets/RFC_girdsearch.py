c0a2047403fd98fe9391d4824ef31268a4ede481
Udbhav Pangotra <udbhavpangotra@ymail.com>
2020-02-03 14:21:43 +0530

    updated RFC_GridSearch.py
    Added a few comments for easier use.

--- code - snippets/RFC_girdsearch.py
+++ code - snippets/RFC_girdsearch.py
@@ -6,8 +6,9 @@ rfc=RandomForestClassifier(random_state=42)
 # use randomized search first and then minimise the grid search, fine tune each of these paramerters using grid search.
 
 param_grid = { 
-    'n_estimators': [100, 200, 500, 750, 1000, 1250, 1500, 2000], #The number of trees in the forest
-    'max_features': ['auto', 'sqrt', 'log2', 3, 5, 7, 9], #The number of features to consider when looking for the best split
+    'n_estimators': [64, 84, 100, 128, 200, 256, 500, 750, 1000, 1250, 1500, 2000], #The number of trees in the forest
+     # when we are working with a higer number of records we should prefer using a lower number of trees and vice versa.
+    'max_features': ['auto', 'sqrt', 'log2', 3, 5, 7, 9, 12], #The number of features to consider when looking for the best split
     'max_depth' : [4, 5, 6, 7, 8, 9, 10, 'None'], # The maximum depth of the tre
     'criterion' :['gini', 'entropy'], # entropy is for information gain
     'min_samples_split' :[2, 3, 4, 5], # The minimum number of samples required to split an internal node
@@ -21,3 +22,8 @@ CV_rfc.fit(X_train, y_train)
 
 
 CV_rfc.best_params_
+
+# an important aspect of using a Grid Search is that we should do a minimaliistic gridsearch rahter than a detailed one, this enables us to use gird search as a fine tuning method rather than a guess method.
+# also, its better if we just use the major parameters first and then keep on adding a few others one after the other.
+# this ensures a faster and a more robust model building.
+# so for the first iteration, start with the n_estimators, max_depth and criterion for a classification problem.
